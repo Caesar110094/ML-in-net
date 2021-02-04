51fb75a5126ecbb1c5324f3451a450288df5a780
cfenc <cfenc@DESKTOP-QFJHHIJ>
2021-02-04 21:20:19 +0100

    Opdracht 1 af

--- ML in netML.ConsoleApp/Program.cs
+++ ML in netML.ConsoleApp/Program.cs
@@ -9,22 +9,28 @@ namespace ML_in_netML.ConsoleApp
     {
         static void Main(string[] args)
         {
-            // Create single instance of sample data from first line of dataset for model input
-            ModelInput sampleData = new ModelInput()
+            Console.WriteLine("Do you want to analyse a sentence?");
+            String varAnswer = Console.ReadLine();
+            while (varAnswer.ToLower() == "yes")
             {
-                SentimentText = @"==RUDE== Dude, you are rude upload that carl picture back, or else.",
-                LoggedIn = @"TRUE",
-            };
+                Console.WriteLine("What sentence do you want to analyse?");
+                String varSentence = Console.ReadLine();
+                Console.WriteLine(varSentence);
 
-            // Make a single prediction on the sample data and print results
-            var predictionResult = ConsumeModel.Predict(sampleData);
+                // Add input data
+                var input = new ModelInput();
+                input.SentimentText = varSentence;
 
-            Console.WriteLine("Using model to make single prediction -- Comparing actual Sentiment with predicted Sentiment from sample data...\n\n");
-            Console.WriteLine($"SentimentText: {sampleData.SentimentText}");
-            Console.WriteLine($"LoggedIn: {sampleData.LoggedIn}");
-            Console.WriteLine($"\n\nPredicted Sentiment value {predictionResult.Prediction} \nPredicted Sentiment scores: [{String.Join(",", predictionResult.Score)}]\n\n");
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-            Console.ReadKey();
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine($"Based on what I have learned, the analysis returns a toxic value of: {result.Prediction}");
+
+                Console.WriteLine("Do you want to analyse another sentence?");
+                varAnswer = Console.ReadLine();
+
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
         }
     }
 }
