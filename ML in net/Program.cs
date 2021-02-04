ab14ebadc82deb1be58c5ec463dd18ccfbea7c42
cfenc <cfenc@DESKTOP-QFJHHIJ>
2021-02-04 21:12:56 +0100

    Opdracht 1

---
+++ ML in net/Program.cs
@@ -0,0 +1,19 @@
+﻿using ML_in_netML.Model;
+using System;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "Next be nicer.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
